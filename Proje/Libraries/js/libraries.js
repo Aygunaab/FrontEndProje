d0e270d341f70ac2ca267594286915033336cd1f
unknown <aygunaab@code.edu.az>
2021-02-06 16:11:30 -0800

    index

--- Proje/Libraries/js/libraries.js
+++ Proje/Libraries/js/libraries.js
@@ -1,71 +1,83 @@
-
-
-
-var Yle = {
-    init: function() {
-        this.Basic.init();  
-    },
-    Basic: {
-        init: function() {
-            this.ylpreloader();
-            this.BackgroundImage();
-            this.Animation();
-            this.ylStickymenu();
-            this.ylMobileMenu();
-            this.YlVideoBox();
-            this.ylSliderMain();
-            this.YlDepartment();
-            this.YlTestimonial1();
-            this.CourseGrid();
-            this.CourseGridFilter();
-            this.countDown();
-            this.Ylblog();
-            this.YLcategory();
-            this.YlTestimonial12();
-            this.YlTeamInstructor();
-            this.YlBlog2();
-            this.GoogleMap();
-
-        },
-
-
-
-
-
-
-
-    GoogleMap: function (){
-    if ( $('#map').length ){
-        var $lat = $('#map').data('lat');
-        var $lon = $('#map').data('lon');
-        var $zoom = $('#map').data('zoom');
-        var $marker = $('#map').data('marker');
-        var $info = $('#map').data('info');
-        var $markerLat = $('#map').data('mlat');
-        var $markerLon = $('#map').data('mlon');
-        var map = new GMaps({
-            el: '#map',
-            lat: $lat,
-            lng: $lon,
-            scrollwheel: false,
-            scaleControl: true,
-            streetViewControl: false,
-            panControl: true,
-            disableDoubleClickZoom: true,
-            mapTypeControl: false,
-            zoom: $zoom,
-        });
-        map.addMarker({
-            lat: $markerLat,
-            lng: $markerLon,
-            icon: $marker,    
-            infoWindow: {
-                content: $info
-            }
+$(function(){
+  $('.slider').owlCarousel({
+    items: 4,
+          margin:10,
+          responsiveClass:true,
+					loop: true,
+					nav: true,
+					dots: true,
+					autoplay: false,
+					navSpeed: 1000,
+					smartSpeed: 2000,
+					navText:["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+          responsive:{
+            0:{
+              items:1,
+            },
+            400:{
+              items:1,
+            },
+            600:{
+              items:2,
+            },
+            700:{
+              items:2,
+            },
+            1000:{
+              items:3,
+      
+            },
+            1300:{
+              items:3,
+      
+            },
+            1900:{
+              items:3,
+            },
+          },
         })
-    }
-}
-    }
-}
-        
-        
+})
+$(function(){
+  
+  $('.testimonials-slider').owlCarousel({
+    items: 4,
+          margin:30,
+          responsiveClass:true,
+          loop: true,
+          nav: true,
+          dots: true,
+          autoplay: false,
+          navSpeed: 1000,
+          smartSpeed: 2000,
+          navText:["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+          responsive:{
+            0:{
+              items:1,
+            },
+            400:{
+              items:1,
+            },
+            600:{
+              items:2,
+            },
+            700:{
+              items:2,
+            },
+            1000:{
+              items:3,
+      
+            },
+            1300:{
+              items:4,
+      
+            },
+            1900:{
+              items:4,
+            },
+          },
+        })
+})
+
+
+
+
